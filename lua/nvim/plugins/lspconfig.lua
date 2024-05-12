9c077c5a484de4dc075ad41fcfa345f73f415051
sarveen <sarveensanthiramohan@gmail.com>
2024-05-12 22:33:19 +0100

    Add nvim-cmp for autocomplete

--- lua/nvim/plugins/lspconfig.lua
+++ lua/nvim/plugins/lspconfig.lua
@@ -1,6 +1,11 @@
 local function setup()
     local lspconfig = require('lspconfig')
 
+    local mason_lspconfig = require("mason-lspconfig")
+
+    local cmp_nvim_lsp = require("cmp_nvim_lsp")
+
+
     -- Function to setup LSP servers with common settings
     local function on_attach(client, bufnr)
         -- Key mappings and other buffer-specific settings
@@ -13,10 +18,19 @@ local function setup()
         buf_set_keymap('n', 'gR', '<cmd>Telescope lsp_references<CR>', { noremap=true, silent=true })
     end
 
+    local capabilities = cmp_nvim_lsp.default_capabilities()
+
+    local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
+    for type, icon in pairs(signs) do
+      local hl = "DiagnosticSign" .. type
+      vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+    end
+
     -- Setup servers
     local servers = { 'lua_ls', 'pyright', 'tsserver', 'clangd', 'gopls', 'jdtls' }
     for _, lsp in ipairs(servers) do
         lspconfig[lsp].setup {
+            capabilities = capabilities,
             on_attach = on_attach,
             flags = {
                 debounce_text_changes = 150,
@@ -28,8 +42,11 @@ end
 
 return {
     "neovim/nvim-lspconfig",
+    event = { "BufReadPre", "BufNewFile" },
     dependencies = {
-        "williamboman/mason-lspconfig.nvim",
+        "hrsh7th/cmp-nvim-lsp",
+        { "antosha417/nvim-lsp-file-operations", config = true },
+        { "folke/neodev.nvim", opts = {} },
     },
     config = setup
 }
